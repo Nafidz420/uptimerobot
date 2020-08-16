2d98d3f583674faf1c8d9f9e15ea1849ed415789
Nafidz420 <69711792+Nafidz420@users.noreply.github.com>
2020-08-16 17:01:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
-const urls = ["https://cooked-helpful-baryonyx.glitch.me"]
+const urls = ["https://anime-link.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
-            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+            axios.get(url).then(console.log("pingpong at " + Date.now())).catch(() => {});
         })
     }, 60 * 1000);
